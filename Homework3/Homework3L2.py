03d0a035f1c229f0ffa0021c19dc24018ef129db
lipu02 <546155538@qq.com>
2016-09-25 11:52:09 +0800

    Update Homework3L2.py

--- Homework3/Homework3L2.py
+++ Homework3/Homework3L2.py
@@ -1,15 +1,15 @@
 blank=[[' ' for j in range (40)]for i in range (30)]
 blank[25][13:26]='A',' ','m','o','v','i','n','g',' ','h','e','a','r','t'
-heart=[[' ' for j in range (9)]for i in range (8)]
+A=[[' ' for j in range (9)]for i in range (8)]
 for j in [1,2,6,7]:
-    heart[0][j]='#'
+    A[0][j]='#'
 for j in [0,3,5,8]:
-    heart[1][j]='#'
+    A[1][j]='#'
 for j in [0,4,8]:
-    heart[2][j]='#'
+    A[2][j]='#'
 for i in range(5):
-    heart[3+i][i]='#'
-    heart[7-i][i+4]='#'
+    A[3+i][i]='#'
+    A[7-i][i+4]='#'
 f=open("moving_heart.txt",'wb')
 for k in range(30):
     for i in range(8):
@@ -25,6 +25,9 @@ for k in range(30):
         f.write('\n')
         for j in range(40):
             f.write(blank[i][j])
+for a = "A"
+l = list(a)
+for a = "".join(l)            
 #clear
     for i in range(8):
         for j in range(9):
